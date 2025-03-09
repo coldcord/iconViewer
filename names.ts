eecb20bcaee7e4fe058f805911ca46686a43ec2a
programminglaboratorys <programming.laboratorys@gmail.com>
2025-03-10 01:31:55 +0300

    feat: in-work search menu chore: bump icon names

--- names.ts
+++ names.ts
@@ -15,8 +15,9 @@ export const IconsFinds = {
     InstagramNeutral: "4.12.07Zm.1 2c-",
     YoutubeNeutral: "11.5s0 3.95.5 5.85a3.",
     FacebookNeutral: "2.8V12h2.8V9",
+    NintendoSwitchNeutral: "14V2.32c0",
     Pencil: "0-2.82 0l-1.38 1.38a1",
-    AngleBracketsIcon: "0-.4.8v1.98c0",
+    AngleBrackets: "0-.4.8v1.98c0",
     NitroWheel: "2h3a1 1 0 1 1 0 2H5.5",
     Bill: "75.34.75.75C6 7.99 5 9",
     Chat: "2.2 22H12Z\",",
@@ -27,11 +28,13 @@ export const IconsFinds = {
     ChatPlus: "1.24-.37V12a10 10 0 1 ",
     Bug: "1.1.27.1.37 0a6.66 6.6",
     B: "9V2.9c0-.5.4-.9.9-.9h7",
-    EyeIcon: "19.1 21 12 21c-7.11 0-",
-    EyeSlashIcon: "2.63-2.63c",
+    Eye: "19.1 21 12 21c-7.11 0-",
+    EyeSlash: "2.63-2.63c",
     EyePlus: "3.32ZM19 14a1 ",
     Heart: "0C9.43 20.48 1 15.09 1",
     Star: ".73-2.25h6.12l1.9-5.83Z",
+    StarOutline: "3.1h5.26l1.62",
+    StarShooting: "1.35l2.95 2.14",
     QrCode: "0v3ZM20",
     Friends: "12h1a8",
     PlusSmall: "0v-5h5a1",
@@ -40,13 +43,31 @@ export const IconsFinds = {
     PinUpright: "5H8v4.35l-3.39",
     PinUprightSlash: "1.56ZM11.08 ",
     ArrowsLeftRight: "18.58V3a1",
-    XSmallIcon: "13.42l5.3",
-    CircleX: "22Zm4.7-15.7a1",
+    XSmall: "13.42l5.3",
+    XLarge: "13.42l7.3 7.3Z",
+    XSmallBold: "12l4.94-4.94a1.5",
+    XLargeBold: "12l6.94-6.94a1.5",
+    Lock: "3Zm9-3v3H9V6a3",
+    LockUnlocked: "1-1.33-1.5ZM14",
+    Video: "1.45-.9V7.62a1",
+    VideoSlash: "1.4l20-20ZM9.2",
+    VideoLock: "1.32-.5V7.62a1",
+    Fire: "14Zm9.26-.84a.57.57",
+    Warning: "3.15H3.29c-1.74",
+    Download: "1.42l3.3 3.3V3a1",
+    Upload: "0ZM3 20a1 1",
+    // QuestionMark: "0ZM5.5 7a1.5" Unknown name
     Quest: "10.47a.76.76",
     Play: "4.96v14.08c0",
     Emoji: " 0 0 0 0 22ZM6.5",
     Gif: "3H5Zm2.18 13.8",
     Trash: "2.81h8.36a3",
+    Bell: "9.5v2.09c0",
+    Screen: "0-3-3H5ZM13.5",
+    ScreenArrow: "3V5Zm16",
+    ScreenStream: " 2-2h3a2",
+    ScreenSystemRequirements: "3V5Zm3", // a guess
+    ScreenSlash: "5.8ZM17.15",
     ScreenX: "1-3-3V5Zm6.3.3a1",
     Plus: "0v8H3a1 1 0 1 0 0 2h8v8a1",
     Id: "15h2.04V7.34H6V17Zm4",
@@ -74,7 +95,7 @@ export const IconsFinds = {
     ChatArrowRight: "2.43l.06",
     Bookmark: "1-1.67.74l",
     ChannelList: "1-1-1ZM2 8a1",
-    ChannelListMagnifyingGlassIcon: "2h18a1 1 0 1 0 0-2H3ZM2",
+    ChannelListMagnifyingGlass: "2h18a1 1 0 1 0 0-2H3ZM2",
     Activities: "1h3a3 3 0 0 0 3-3Z\"",
     ActivitiesPlus: "14.35v1.29a",
     AnnouncementsLock: "1-2.46-1.28 3.86",
@@ -84,7 +105,36 @@ export const IconsFinds = {
     ShieldUser: "9.77V6.75c0-.57.17",
     ShieldAt: "14.42-.35.75",
     Shield: "M4.27 5.22A2.66", // a guess
+    Slash: "1-.43-.76L15.78",
+    SlashBox: "0-3-3H5Zm12.79",
+    Apps: "2.95H20a2 2 0",
+    CheckmarkLarge: "1.4l-12 12a1",
+    CheckmarkLargeBold: "2.12-2.12L9",
+    CheckmarkSmallBold: "13.88l6.94-6.94a1.5",
+    CheckmarkSmall: "1-1.4 0l-4-4a1",
+    NewUser: "0-.92h-.03a2", // a guess
+    UserCheck: "0l1.8-1.8c.17",
+    User: "2.9.06.24.26.",
+    UserMinus: "3-3h5.02c.38",
+    UserPlus: "2.07ZM12",
+    UserPlay: "0-3.61-.71h-.94Z",
+    UserBox: "0-3-3H5Zm10 6a3", // a guess
+    Settings: "0ZM16 12a4",
+    SettingsInfo: "10Zm1-4a1",
+    Hashtag: "8 4.84a1", // a guess
+    HashtagLocked: "2.02.31.03", // a guess
+    HashtagWarning: "8h1.26Z", // a guess
+    HashtagPlay: "52.88H9.85l", // a guess
+    Flag: "5.85v7.3a2",
+    Language: "5.43h3.85l",
+    Lightbulb: "8.5ZM15.1 19c.5",
+    Key: "23-.24ZM10 16a2",
+    InBox: "3H5ZM4 5.5C4",
+    BookmarkOutline: "0-1-1ZM7 2a3",
+    Food: "7.58V8a1 1"
 };
+
+// 13l4.91-8.05a1.8
 
 export const namePatterns = new Map(Object.entries(IconsFinds).map(([name, pattern]) => [name, pattern]));
 
--- subComponents.tsx
+++ subComponents.tsx
@@ -10,26 +10,31 @@
 //                <IconColorTooltip color={color} />
 //            </Text>
 
+import { getIntlMessage } from "@utils/discord";
 import { Margins } from "@utils/margins";
 import { classes } from "@utils/misc";
-import { Clickable, Text, TooltipContainer } from "@webpack/common";
+import { Clickable, ContextMenuApi, FluxDispatcher, Menu, Text, TooltipContainer, useState } from "@webpack/common";
+import type { ComponentPropsWithRef, PropsWithChildren } from "react";
 
 import { cssColors } from "./utils";
 
 
-export function IconTooltip({ children, copy, className }: { children: string; copy: string; className?: string; }) {
+
+export type ClickableProps<T extends "a" | "div" | "span" | "li" = "div"> = PropsWithChildren<ComponentPropsWithRef<T>> & {
+    tag?: T;
+};
+
+export function IconTooltip({ children, copy, className, ...props }: ClickableProps & { children: string; copy: string; }) {
     return <TooltipContainer text={"Click to copy"} className={className}>
         <Clickable onClick={() => {
             // @ts-ignore
             Clipboard.copy(copy);
-        }}
-            onContextMenu={() => {
-
-            }}>{children}</Clickable>
+        }} {...props}>{children}</Clickable>
     </TooltipContainer>;
 }
 
 export const ModalHeaderTitle = ({ iconName, color, name }: { iconName: string; color: number; name: string; }) => {
+    const [query, setQuery] = useState("");
     return <Text variant="heading-lg/semibold"
         style={{ flexGrow: 1, display: "flex" }}
         className={classes("vc-ic-modal-header-title", `vc-ic-${name}-modal-header-title`)}>
@@ -37,10 +42,35 @@ export const ModalHeaderTitle = ({ iconName, color, name }: { iconName: string; 
             {iconName}
         </IconTooltip>
         {" - "}
-        <IconTooltip copy={cssColors[color]?.css} className={classes(Margins.left8, "vc-icon-modal-color-tooltip")}>
+        <IconTooltip copy={cssColors[color]?.css} className={classes(Margins.left8, "vc-icon-modal-color-tooltip")}
+            onContextMenu={e => {
+                ContextMenuApi.openContextMenu(e, () => (
+                    <Menu.Menu
+                        navId="vc-pindms-header-menu"
+                        onClose={() => FluxDispatcher.dispatch({ type: "CONTEXT_MENU_CLOSE" })}
+                        color="danger"
+                        aria-label="Icon Viewer Colors"
+                    >
+                        <Menu.MenuControlItem
+                            id="vc-ic-colors-search"
+                            control={(props, ref) => (
+                                <Menu.MenuSearchControl
+                                    {...props}
+                                    query={query}
+                                    onChange={setQuery}
+                                    ref={ref}
+                                    placeholder={getIntlMessage("SEARCH")}
+                                />
+                            )}
+                        />
+
+                    </Menu.Menu>
+                ));
+            }}>
             {cssColors[color]?.name}
         </IconTooltip>
-    </Text>;
+    </Text >;
 };
 
+// {cssColors[color]?.name}
 
